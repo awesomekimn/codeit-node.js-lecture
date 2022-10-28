4b0331ee981e00bed16f9205f62a33f47e78ab36
awesomekimn <awesomekimn@gmail.com>
2022-10-28 23:12:08 +0900

    '/' 내용 수정

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ const { Member } = db;
 
 app.use(express.json()); // express.json() : middleware
 
+app.get('/', (req, res) => {
+  res.send('URL should contain /api/..');
+});
+
 app.get('/api/members', async (req, res) => {
   const { team } = req.query;
   if (team) {
